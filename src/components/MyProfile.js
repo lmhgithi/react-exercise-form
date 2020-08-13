05d52638467166a6bb9ec5b74c8fce40b5841e8c
lmhgithi <lmhfox@foxmail.com>
2020-08-13 20:51:56 +0800

    [liminghao] fix submit disabled

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,61 +1,67 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './myProfile.less';
 
 class MyProfile extends Component {
-    state = {
-        name: '',
-        gender: 'male',
-        description: '',
-        checkReadTerms: '',
-    }
+  state = {
+    name: '',
+    gender: 'male',
+    description: '',
+    checkReadTerms: '',
+  }
 
-    handleChange = (field, event) => {
-        this.setState({
-            [field]: event.target.value,
-        })
-    }
-    handleSubmit = () => {
-        console.log(this.state)
-        event.preventDefault()
-    }
+  handleChange = (field, event) => {
+    this.setState({
+      [field]: event.target.value,
+    })
+  }
+  handleSubmit = () => {
+    console.log(this.state)
+    event.preventDefault()
+  }
+
+  handleChangeCheck = () => {
+    this.setState({
+      checkReadTerms: event.target.checked,
+    })
+  }
   render() {
     return (
-        <div className='myProfileForm'>
-          <h1>My Profile</h1>
-          <form onSubmit={this.handleSubmit}>
-            <label htmlFor='name'>Name</label>
-            <input type='text' name = 'name'
-                   value={this.state.name}
-                   placeholder='Your name'
-                   onChange={() => {this.handleChange('name', event)}}
-            />
-            <label htmlFor='gender'>Gender</label>
-            <select name = 'gender'
-                    onChange={() => {this.handleChange('gender', event)}}
-                    value={this.state.gender}
-            >
-                <option value="male">Male</option>
-                <option value="female">Female</option>
-                <option value="male">Other</option>
-            </select>
-            <label htmlFor='description'>Description</label>
-            <textarea name = 'description' placeholder='Description about yourself'
-                      onChange={() => {this.handleChange('description', event)}}
-                      value={this.state.description}
-            />
-            <input className='checkboxInput' type='checkbox' name = 'checkReadTerms'
-                   onChange={() => {this.handleChange('checkReadTerms', event)}}
-                   value={this.state.checkReadTerms}
-            />
-            <label className='checkboxLabel' htmlFor='checkReadTerms'>I have read the terms of conduct</label>
-            <input  className='submitButton' type='submit' name='Submit' value='Submit'
-                   disabled={!this.state.name ||
-                   !this.state.checkReadTerms ||
-                   !this.state.description ||
-                   !this.state.gender}
-            />
-          </form>
-        </div>
+      <div className='myProfileForm'>
+        <h1>My Profile</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor='name'>Name</label>
+          <input type='text' name='name'
+            value={this.state.name}
+            placeholder='Your name'
+            onChange={() => { this.handleChange('name', event) }}
+          />
+          <label htmlFor='gender'>Gender</label>
+          <select name='gender'
+            onChange={() => { this.handleChange('gender', event) }}
+            value={this.state.gender}
+          >
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="male">Other</option>
+          </select>
+          <label htmlFor='description'>Description</label>
+          <textarea name='description' placeholder='Description about yourself'
+            onChange={() => { this.handleChange('description', event) }}
+            value={this.state.description}
+          />
+          <input className='checkboxInput' type='checkbox' name='checkReadTerms'
+            onChange={this.handleChangeCheck}
+            value={this.state.checkReadTerms}
+          />
+          <label className='checkboxLabel' htmlFor='checkReadTerms'>I have read the terms of conduct</label>
+          <input className='submitButton' type='submit' name='Submit' value='Submit'
+            disabled={!this.state.name ||
+              !this.state.checkReadTerms ||
+              !this.state.description ||
+              !this.state.gender}
+          />
+        </form>
+      </div>
     );
   }
 }
