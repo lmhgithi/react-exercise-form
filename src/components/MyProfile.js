8b73cd2db833e9ab8f74182d7a234ccbee07c30a
lmhgithi <lmhfox@foxmail.com>
2020-08-13 10:18:55 +0800

    [liminghao] fix value binding

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -3,7 +3,7 @@ import './myProfile.less';
 
 class MyProfile extends Component {
     state = {
-        name: 'myName',
+        name: '',
         gender: 'male',
         description: '',
         checkReadTerms: '',
@@ -25,13 +25,14 @@ class MyProfile extends Component {
           <form onSubmit={this.handleSubmit}>
             <label htmlFor='name'>Name</label>
             <input type='text' name = 'name'
-                   // value={this.state.name}
+                   value={this.state.name}
                    placeholder='Your name'
                    onChange={() => {this.handleChange('name', event)}}
             />
             <label htmlFor='gender'>Gender</label>
             <select name = 'gender'
                     onChange={() => {this.handleChange('gender', event)}}
+                    value={this.state.gender}
             >
                 <option value="male">Male</option>
                 <option value="female">Female</option>
@@ -40,9 +41,11 @@ class MyProfile extends Component {
             <label htmlFor='description'>Description</label>
             <textarea name = 'description' placeholder='Description about yourself'
                       onChange={() => {this.handleChange('description', event)}}
+                      value={this.state.description}
             />
             <input className='checkboxInput' type='checkbox' name = 'checkReadTerms'
                    onChange={() => {this.handleChange('checkReadTerms', event)}}
+                   value={this.state.checkReadTerms}
             />
             <label className='checkboxLabel' htmlFor='checkReadTerms'>I have read the terms of conduct</label>
             <input  className='submitButton' type='submit' name='Submit' value='Submit'
